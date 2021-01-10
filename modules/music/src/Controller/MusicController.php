98b391bb090ef2ac57be31f9b9d24bcc4d2e51bc
Tsvetozar Bonev <tsbonev@gmail.com>
2021-01-10 18:51:33 +0200

    add schema

--- modules/music/src/Controller/MusicController.php
+++ modules/music/src/Controller/MusicController.php
@@ -4,10 +4,10 @@ namespace Drupal\music\Controller;
 
 class MusicController
 {
- public function hello() {
-  return array(
-    '#title' => 'Hello world!',
-    '#markup' => 'This is some content.'
-   );
- }
+  public function listAlbums() {
+    return array(
+      '#title' => 'Hello world!',
+      '#markup' => 'This is some content.'
+    );
+  }
 }
